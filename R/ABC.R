ca36459bb39bd7f95bb125afc0077e1c25ba7ee8
vovkaOst <vovaostruki@gmail.com>
2015-12-28 16:37:29 +0100

    2. Next step to metaheuristic pattern

--- R/ABC.R
+++ R/ABC.R
@@ -1,22 +1,26 @@
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
-#To define the METHOD completely the user must
-#code the procedures of selection, model update, and variation.
-#Proper execution of the metaheuristic method needs
-#a list of start points, an evaluation method
-#an initialization procedure, and a termination condition
-
-############################################################
-#rm(list=ls())
-
+#' Implementation of Artificial Bee Colony(ABC) algorithm
+#' for the purposes of ALHE class on Warsaw University of Technology
+#'
+#' Finds a local minimums of the function(aka "the best food sources")
+#'
+#' @param goal f(x) f(x) - funkcja celu obliczana dla argumentu x
+#' @param dim wymiarowosc dla ktorej prowadzone beda obliczenia
+#' @param par # wektor z parametrami dla danej konkretnej funkcji
+#'
+#' @return res - wynik działania metody, przy czym:
+#'  - res$x to wektor odpowiadający najlepszemu rozwiązaniu,
+#'  -res$y to wartość f. celu dla tego wektora
+#'
+#' @examples
+#' abc(function(x) {100 * (x[2] - x[1]^2)^2 + (1 - x[1])^2}, 2, c(1,1), lb=-2.028, ub=2.028, criter=200)
+#'
+#' @export
 abc <- function(
-  goal,                # f(x) - funkcja celu obliczana dla argumentu x
-  dim,                 # wymiarowosc dla ktorej prowadzone beda obliczenia
+  goal,
+  dim,
 #  pars = ls(               # obiekt z parametrami metody
-    par,               # wektor z parametrami dla danej funkcji
-    D=length(par),     # ilosc(wymiarowosc parametrow) - dla sprawdzenia
+    par,
+ #   D=length(par),     # ilosc(wymiarowosc parametrow) - dla sprawdzenia
     ...,               # argumenty funckcji (M, x0, X, etc.)
     NP = 40,           # ilosc pszczol
     FoodNumber = NP/2, # ilosc zrodl jedzenia
@@ -24,43 +28,42 @@ abc <- function(
     ub = +Inf,         # gorna granica parametrow - dla przyspieszenia
     limit = 100,       # zrodlo jedzenia, ktore nie moze byc ulepszone w tylu probach jest porzucane przez pszczoly
     maxCycle = 1000,   # maksymalna ilosc cykli(iteracji) - stop kryterium
-    optiinteger=FALSE, # TRUE jezeli chcemy optymalizowac binarnie [1,0]
+    optbin=FALSE, # TRUE jezeli chcemy optymalizowac binarnie [1,0]
     criter=50
  # )
 )
 {
 
   # Checking limits
-  if (length(lb)>0) lb <- rep(lb, D)
-  if (length(ub)>0) ub <- rep(ub, D)
-
+  if (length(lb)>0) lb <- rep(lb, dim)
+  if (length(ub)>0) ub <- rep(ub, dim)
   lb[is.infinite(lb)] <- -(.Machine$double.xmax*1e-10)
   ub[is.infinite(ub)] <- +(.Machine$double.xmax*1e-10)
 
   # Initial params
-  Foods       <- matrix(double(FoodNumber*D), nrow=FoodNumber)
-  f           <- double(FoodNumber)
-  fitness     <- double(FoodNumber)
-  trial       <- double(FoodNumber)
-  prob        <- double(FoodNumber)
-  solution    <- double(D)
+  Foods       <- matrix(double(FoodNumber*dim), nrow=FoodNumber)
+  f           <- double(FoodNumber)  #point - history wartosc f-ji celu dla danego rozwiazania
+  fitness     <- double(FoodNumber)  #point - history quality of the point -
+  trial       <- double(FoodNumber)  #point - history
+  prob        <- double(FoodNumber)  #point - history
+  solution    <- double(dim)  #model
   ObjValSol   <- double(1)
   FitnessSol  <- double(1)
   neighbour   <- integer(1)
   param2change<- integer(1)
-  GlobalMin   <- double(1)
-  GlobalParams<- double(D)
+  GlobalMin   <- double(1)      #model
+  GlobalParams<- double(dim)    #model
   #GlobalMins  <- double(runtime)
   r           <- integer(1)
 
   # Fun
-  fun <- function(par) goal(par, ...)
+ # fun <- function(par) goal(par, ...)
 
-  # Fitness function
-  evaluation <- function(fun)
+  # Fitness function - calculates quality
+  evaluation <- function(f_wart)
   {
-    if (fun >= 0) return(1/(fun + 1))
-    else return(1 + abs(fun))
+    if (f_wart >= 0) return(1/(f_wart + 1))
+    else return(1 + abs(f_wart))
   }
   # evaluation(f[1])
 
@@ -86,17 +89,17 @@ abc <- function(
   # Counters of food sources are also initialized in this function
 
   init <- function(index, firstinit=FALSE, ...) {
-    if (optiinteger) Foods[index,] <<- runif(D) > .5
+    if (optbin) Foods[index,] <<- runif(dim) > .5
     else {
       if (!firstinit) {
-        Foods[index,] <<- sapply(1:D, function(k) runif(1,lb[k],ub[k]) )
+        Foods[index,] <<- sapply(1:dim, function(k) runif(1,lb[k],ub[k]) ) #uniform distr rand deriv
       }
       else {
         # For the first initialization we set the bees at
         # specific places equaly distributed through the
         # bounds.
         Foods[index,] <<-
-          sapply(1:D, function(k) {
+          sapply(1:dim, function(k) {
             seq(lb[k],ub[k],length.out=FoodNumber)[index]
           }
           )
@@ -105,7 +108,7 @@ abc <- function(
 
     solution <<- Foods[index,]
 
-    f[index] <<- fun(solution)
+    f[index] <<- goal(solution)
 
     fitness[index] <<- evaluation(f[index])
     trial[index] <<- 0
@@ -114,11 +117,12 @@ abc <- function(
   # init(2)
 
   # All food sources are initialized
-  initial <- function(firstinit=FALSE) {
-    sapply(1:FoodNumber, init, firstinit=firstinit)
+  initialize <- function(Foods) {
+    sapply(1:FoodNumber, init, firstinit=TRUE)
 
     GlobalMin <<- f[1]
     GlobalParams <<- Foods[1,]
+    return (list(Foods, fitness, trial, prob))
   }
 
   # initial()
@@ -128,7 +132,7 @@ abc <- function(
     for (i in 1:FoodNumber) {
       r <- runif(1)
       # The parameter to be changed is determined randomly
-      param2change <- floor(r*D) + 1
+      param2change <- floor(r*dim) + 1
 
       # A randomly chosen solution is used in producing a mutant solution of the solution i
       neighbour <- floor(r*FoodNumber) + 1
@@ -142,7 +146,7 @@ abc <- function(
       # v_{ij}=x_{ij}+\phi_{ij}*(x_{kj}-x_{ij})
       r <- runif(1)
 
-      if (optiinteger) solution[param2change] <<- r > 0.5
+      if (optbin) solution[param2change] <<- r > 0.5
       else {
         solution[param2change] <<-
           Foods[i,param2change]+
@@ -156,14 +160,14 @@ abc <- function(
           solution[param2change]<<-ub[param2change]
       }
 
-      ObjValSol <<- fun(solution)
+      ObjValSol <<- goal(solution)
       FitnessSol <<- evaluation(ObjValSol)
 
       # a greedy selection is applied between the current solution i and its mutant*/
       if (FitnessSol>fitness[i]) {
         # If the mutant solution is better than the current solution i, replace the solution with the mutant and reset the trial counter of solution i*/
         trial[i] <<- 0;
-        #for(j in 1:D) Foods[i,j] <<- solution[j]
+        #for(j in 1:dim) Foods[i,j] <<- solution[j]
         Foods[i,] <<- solution
         f[i]<<- ObjValSol
         fitness[i]<<-FitnessSol
@@ -204,7 +208,7 @@ abc <- function(
         r <- runif(1)
 
         # The parameter to be changed is determined randomly
-        param2change <- floor(r*D) + 1
+        param2change <- floor(r*dim) + 1
 
         # A randomly chosen solution is used in producing a mutant solution of the solution i
         neighbour <- floor(r*FoodNumber) + 1
@@ -218,7 +222,7 @@ abc <- function(
         # v_{ij}=x_{ij}+\phi_{ij}*(x_{kj}-x_{ij}) */
         r <- runif(1)
 
-        if (optiinteger) solution[param2change] <<- r > .5
+        if (optbin) solution[param2change] <<- r > .5
         else
         {
           solution[param2change] <<-
@@ -234,7 +238,7 @@ abc <- function(
 
         }
 
-        ObjValSol <<- fun(solution)
+        ObjValSol <<- goal(solution)
         FitnessSol <<- evaluation(ObjValSol)
 
         # a greedy selection is applied between the current solution i and its mutant*/
@@ -253,6 +257,7 @@ abc <- function(
       if (i==FoodNumber) i <- 1
       # end of onlooker bee phase
     }
+    return(Foods)
   }
 
   # determine the food sources whose trial counter exceeds the "limit" value.
@@ -268,33 +273,66 @@ abc <- function(
   }
 
 
-#start of main things
-  persistance <- 0
+ selection <- function(history, model){
+   sel <- SendOnlookerBees()
+   return(sel)
+ }
 
-  # Inicializa funcion
-  initial(firstinit=T)
-
-  # Memoriza la primera mejor solucion
+initModel <- function(history){
   MemorizeBestSource()
+  SendEmployedBees()
+  evaluateList()
+  return(GlobalParams)
+}
+
+
+modelUpdate <- function(selectedPoints, oldModel){
+  MemorizeBestSource()
+  return(GlobalParams)
+}
+
+variation <- function(selectedPoints, newModel){
+  SendScoutBees()
+  SendEmployedBees()
+  return(Foods)
+}
+#push a LIST of points into the history
+historyPush<-function(oldHistory, newPoints)
+{
+  #    newHistory<-c(oldHistory,newPoints)
+  #    return (newHistory)
+  return(Foods)
+}
+#read a LIST of points pushed recently into the history
+historyPop<-function(history, number)
+{
+  stop=length(history)
+  start=max(stop-number+1,1)
+  return(history[start:stop])
+}
+
+  persistance <- 0
+  history <- initialize(Foods) #return initialized foods - points which bees will estimate
+  model <- initModel(history)
 
   iter <- 0
-  # Comienza a iterar
+  #
   while ((iter <- iter + 1) < maxCycle)
   {
-    SendEmployedBees()            #selection
+    selectedPoints <- selection(history, model)  #agrop
+    newModel <- modelUpdate(selectedPoints, model)  #agrop
+    if (persistance > criter) break     #opcjonalne - optymalizacja przy tluczeniu sie w tym samym miejscu
+    newPoints <- variation(selectedPoints, newModel)              #agrop
+
     evaluateList()
-    SendOnlookerBees()
-    MemorizeBestSource()          #model update
-    if (persistance > criter) break
-    SendScoutBees()               #variation?
+    history <- historyPush(history, newPoints)
+    model <- newModel
   }
 
   return(
     list(
       x=GlobalParams,
-      y=fun(GlobalParams)
-      #counts=c("function"=iter)
+      y=goal(GlobalParams)
     )
   )
-
 }
